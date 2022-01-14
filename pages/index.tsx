7a031c042b6551e59432592e1c97a938c0ccb623
shareef <nadeemshareef934@gmail.com>
2022-01-14 12:03:15 +0530

    ✨ add color-picker

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 import Head from "next/head";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { RgbaStringColorPicker } from "react-colorful";
 
 export default function Home() {
+  const [currentColor, setCurrentColor] = useState<"bgColor" | "textColor">(
+    "bgColor"
+  );
+  const [bgColor, setBgColor] = useState<string>("");
+  const [textColor, setTextColor] = useState<string>("");
+
+  const changeColor = () => {
+    if (currentColor === "bgColor") return setCurrentColor("textColor");
+    setCurrentColor("bgColor");
+  };
+
   return (
     <Fragment>
       <Head>
@@ -12,9 +24,34 @@ export default function Home() {
         />
       </Head>
       <main>
-        <p className="text-3xl font-bold underline">
-          A website to help choose color schema for your website/blog
-        </p>
+        <section>
+          <div className="flex justify-center">
+            <div className="color-picker">
+              <RgbaStringColorPicker
+                color={currentColor === "bgColor" ? bgColor : textColor}
+                onChange={
+                  currentColor === "bgColor" ? setBgColor : setTextColor
+                }
+              />
+            </div>
+            <div className="">
+              <div
+                className="w-52 h-52"
+                style={{ backgroundColor: textColor, color: bgColor }}
+                onClick={() => setCurrentColor("textColor")}
+              >
+                <span>Text Color</span>
+              </div>
+              <div
+                className="w-52 h-52"
+                style={{ backgroundColor: bgColor, color: textColor }}
+                onClick={() => setCurrentColor("bgColor")}
+              >
+                <span>Background Color</span>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </Fragment>
   );
